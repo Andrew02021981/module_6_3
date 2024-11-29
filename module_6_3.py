7a5c7f0b97d86406e1e8ecfd5e2f15843679b2a3
Andrew02021981 <andrejsazonov5487@gmail.com>
2024-11-29 16:23:17 +0300

    Update module_6_3.py

--- module_6_3.py
+++ module_6_3.py
@@ -7,19 +7,20 @@ class Animal:
         self.speed = speed
 
     def move(self, dx, dy, dz):
-        self.dx = dx*self.speed
-        self.dy = dy*self.speed
-        self.dz = dz * self.speed
-        if self.dz < 0:
+        if dz < 0:
             print("It's too deep, i can't dive :(")
+        else:
+            self._cords[0] += dx*self.speed
+            self._cords[1] += dy*self.speed
+            self._cords[2] += dz*self.speed
 
     def get_cords(self):
-        print(f' X: {self.dx}, Y: {self.dy}, Z: {self.dz}')
+        print (f' X: {self._cords[0]}, Y: {self._cords[1]}, Z: {self._cords[2]}')
 
     def attack(self):
         if self._DEGREE_OF_DANGER < 5:
             print("Sorry, i'm peaceful :)")
-        elif self._DEGREE_OF_DANGER >= 5:
+        else:
             print("Be careful, i'm attacking you 0_0")
 
     def speak(self):
@@ -35,7 +36,7 @@ class AquaticAnimal(Animal):
     _DEGREE_OF_DANGER = 3
 
     def dive_in(self, dz):
-        self.dz = abs(self.dz-dz*(self.speed/2))
+        self._cords[2] = abs(self._cords[2]-dz*(self.speed/2))
 
 class PoisonousAnimal(Animal):
     _DEGREE_OF_DANGER = 8
